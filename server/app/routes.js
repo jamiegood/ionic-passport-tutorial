e89e83b46c3666ce1e00bce8dadfcc9cb405b342
jamiegood <jamiegood@gmail.com>
2016-11-03 22:27:34 +0000

    Repeat

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,14 +1,36 @@
-var TodoController = require('./controllers/todos');
-var express = require('express');
+var AuthenticationController = require('./controllers/authentication'),
+    TodoController = require('./controllers/todos'),
+    express = require('express'),
+    passportService = require('../config/passport'),
+    passport = require('passport');
 
-module.exports = function(app) {
+var requireAuth = passport.authenticate('jwt', {session: false}),
+    requireLogin = passport.authenticate('local', {session: false});
 
-  var todoroutes = express.Router();
+module.exports = function(app){
 
+    var apiRoutes = express.Router(),
+        authRoutes = express.Router(),
+        todoRoutes = express.Router();
 
-  todoroutes.get('/', TodoController.getTodos);
+    // Auth Routes
+    apiRoutes.use('/auth', authRoutes);
 
+    authRoutes.post('/register', AuthenticationController.register);
+    authRoutes.post('/login', requireLogin, AuthenticationController.login);
 
-  app.use('/api', todoroutes);
+    authRoutes.get('/protected', requireAuth, function(req, res){
+        res.send({ content: 'Success'});
+    });
+
+    // Todo Routes
+    apiRoutes.use('/todos', todoRoutes);
+
+    todoRoutes.get('/', requireAuth, AuthenticationController.roleAuthorization(['reader','creator','editor']), TodoController.getTodos);
+    todoRoutes.post('/', requireAuth, AuthenticationController.roleAuthorization(['creator','editor']), TodoController.createTodo);
+    todoRoutes.delete('/:todo_id', requireAuth, AuthenticationController.roleAuthorization(['editor']), TodoController.deleteTodo);
+
+    // Set up routes
+    app.use('/api', apiRoutes);
 
 }
